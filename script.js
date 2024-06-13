2a0920a1972844dff75111175b9f6336706b11ab
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-13 14:20:32 +0100

    added pet interaction buttons

--- script.js
+++ script.js
@@ -1,26 +1,74 @@
-
-
-
-const gameOver = () => {
-    if (pet.health <= 0) {
-        clearInterval (timer)
-        alert(`${this.name} has died!`)
-    } 
-    else if (pet.hunger <= 0) {
-        clearInterval (timer)
-        alert(`${this.name} has starved to death!`)
-    }
-    else if (pet.happiness <= 0) {
-        clearInterval (timer)
-        alert(`${this.name} ran away...`)
-    }
+let pet = {
+  name: "Fluffy",
+  health: 20,
+  hunger: 20,
+  happiness: 20,
 };
 
+const gameOver = () => {
+  if (pet.health <= 0) {
+    clearInterval(timer);
+    console.log(`${this.name} has died!`);
+  } else if (pet.hunger <= 0) {
+    clearInterval(timer);
+    console.log(`${this.name} has starved to death!`);
+  } else if (pet.happiness <= 0) {
+    clearInterval(timer);
+    console.log(`${this.name} ran away...`);
+  } else if (pet.hunger <= 0 && pet.happiness <= 0) {
+    clearInterval(timer);
+    console.log(`${this.name} ran away from home and later starved to death...`)
+  } 
+};
 
 timer = setInterval(() => {
-    pet.health -=5
-    pet.hunger -=5
-    pet.happiness -=5
+  pet.health -= 5;
+  pet.hunger -= 5;
+  pet.happiness -= 5;
+  gameOver();
 }, 2000);
 
-console.log(timer)
+console.log(timer);
+
+// let threshold = 20; 
+
+// let timer = setInterval(() => {
+//   if (pet.hunger > 0) {
+//     pet.hunger -= 5;
+//   }
+
+//   if (pet.hunger < threshold) {
+//     pet.health -= 5;
+//   }
+
+//   pet.happiness -= 5;
+//   gameOver(); 
+// }, 2000);
+
+//dog buttons
+
+tugBtn = document.getElementById("");
+
+tugBtn.addEventListener("click", () => {
+    pet.tug()
+});
+
+fetchBtn = document.getElementById("");
+
+fetchBtn.addEventListener("click", () => {
+    pet.fetch()
+});
+
+//cat buttons
+
+napBtn = document.getElementById("");
+
+napBtn.addEventListener("click", () => {
+    pet.nap()
+});
+
+scratchBtn = document.getElementById("");
+
+scratchBtn.addEventListener("click", () => {
+    pet.scratch()
+});
