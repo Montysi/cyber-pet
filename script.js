101072da14cfb18077bba9766bf663dfb4c40d5c
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-13 12:09:40 +0100

    simple if/else statement for loss conditions

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 
 
-
 const gameOver = () => {
-    if (yourPet.health <= 0 {
-        
-    })
-}
+    if (pet.health <= 0) {
+        clearInterval (timer)
+        alert(`${this.name} has died!`)
+    } 
+    else if (pet.hunger <= 0) {
+        clearInterval (timer)
+        alert(`${this.name} has starved to death!`)
+    }
+    else if (pet.happiness <= 0) {
+        clearInterval (timer)
+        alert(`${this.name} ran away...`)
+    }
+};
+
+
+timer = setInterval(() => {
+    pet.health -=5
+    pet.hunger -=5
+    pet.happiness -=5
+}, 2000);
+
+console.log(timer)
